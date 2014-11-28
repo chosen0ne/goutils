6b057bc0525cdd4fc99c01d7d2088f7a866c5072
chosen0ne <louzhenlin86@126.com>
2014-11-28 12:00:36 +0800

    first ci

---
+++ errors.go
@@ -0,0 +1,28 @@
+/**
+ *
+ * @author  chosen0ne(louzhenlin86@126.com)
+ * @date    2014/11/28 11:51:28
+ */
+
+package goutils
+
+import (
+    "runtime"
+    "errors"
+    "fmt"
+    "bytes"
+    "strconv"
+)
+
+func NewError(format string, args ...interface{}) error {
+    var buf bytes.Buffer
+    _, fname, lno, ok := runtime.Caller(1)
+    if !ok {
+        fname, lno = "unkown", -1
+    }
+
+    fmt.Fprintf(&buf, format, args...)
+
+    // errors.go:15 => some error
+    return errors.New(fname + ":" + strconv.Itoa(lno) + "=>" + string(buf.Bytes()))
+}
